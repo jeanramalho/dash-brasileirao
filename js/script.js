1a5b2ef590ea6d5c8e4fb3b01751cbdd9ce8db99
jeanramalho <jeanramalho.dev@gmail.com>
2022-04-23 22:37:32 -0300

    Implementa cards com bootstrap para resultados

--- js/script.js
+++ js/script.js
@@ -68,23 +68,40 @@ function getReq(ano, rodada) {
                   console.log(data)
                   output += `
                   
-                  <div class="casa">
-                  <img src="${data[home].escudo}">
-                  <h3>${data[home].equipe}</h3>
-                  <h2>${placarHome()}</h2>
+                  <div class="card mb-3" style="max-width: 540px;">
+
+                    <div class="row g-0">
+                      <div class="col-md-4">
+                        <img src=" ${data[home].escudo}" class="img-fluid rounded-start" alt="...">
+                        <h5 class="card-title"> ${data[home].equipe} <h2>${placarHome()}</h2> </h5>
+                        
+                      </div>
+                      
+
+                      <h1>X</h1>
+                      
+                      
+                      <div class="col-md-4">
+                        
+                        <img src=" ${data[visitante].escudo}" class="img-fluid rounded-start" alt="...">
+                        <h5 class="card-title">${data[visitante].equipe} <h2>${placarVis()}</h2> </h5>                        
+                      </div>
+
+                      <div class="col-md-8">
+                        <div class="card-body">
+                          
+                          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+
+                      </div>
+                    </div>
                   </div>
 
-                  <h1>X</h1> 
 
-                  <div class="visi">
-                  <h2>${placarVis()}</h2>
-                  <img src="${data[visitante].escudo}">
-                  <h3>${data[visitante].equipe}</h3>                  
-                  </div>
-                   
-                  <div class="infoJogo">
+
+
                   
-                  </div>
                   
                   
                   
